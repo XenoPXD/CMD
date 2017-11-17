9c9291969d0bf951fc1a6e4d94057f73390ee483
XenoPXD <pxd2001@yahoo.fr>
2017-11-17 18:14:57 +0100

    Add files via upload

---
+++ src/bean/Action.java
@@ -0,0 +1,27 @@
+package bean;
+
+public class Action {
+
+	private String value;
+	private String[] file;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value.trim();
+	}
+
+	public String[] getFile() {
+		return file;
+	}
+
+	public void setFile(String[] file) {
+		this.file = file;
+	}
+
+	
+	
+	
+}
