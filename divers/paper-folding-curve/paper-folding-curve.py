f15df17dabaa808088376313c49f27c81d48aded
XenoPXD <pxd2001@yahoo.fr>
2018-02-24 19:24:25 +0100

    Add files via upload

--- divers/paper-folding-curve/paper-folding-curve.py
+++ divers/paper-folding-curve/paper-folding-curve.py
@@ -21,15 +21,15 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    sys.stdout.write('%s |%s| %s%% %s \r' % (prefix, bar, percent, suffix))
+    bar = fill * filledLength + '.' * (length - filledLength)
+    sys.stdout.write('%s [%s] %s%% %s \r' % (prefix, bar, percent, suffix))
     sys.stdout.flush()
     #print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
     # Print New Line on Complete
-    if iteration == total: 
-        print()
+    #if iteration == total:
+    #    print()
 
-  
+
 global compteurProgressBar
 compteurProgressBar=0
 
@@ -54,8 +54,8 @@ def path(order, s, e, file):
         curve(order, start, current, end, modulo, file)
         current+=1
         s+=1
-    #sys.stdout.write("\n")
-    
+    sys.stdout.write("\n")
+
 def curve(order, start, current, end, modulo, file):
     global compteurProgressBar
     if current<=end+1:
@@ -67,7 +67,7 @@ def curve(order, start, current, end, modulo, file):
             else:
                 printProgressBar(compteurProgressBar-1, end-start, prefix = 'Progress:', suffix = 'Complete  ', length = 50)
                 compteurProgressBar+=1
-                file.write(value)     
+                file.write(value)
         else:
             curve(order-1, start, current, end, modulo*2, file)
 
@@ -104,7 +104,7 @@ t += "create by java_coffee_cup\n"
 t += "resolved by Xeno_PXD\n"
 
 
-parser = argparse.ArgumentParser(description=t, 
+parser = argparse.ArgumentParser(description=t,
                                  epilog=e, formatter_class=CustomFormatter)
 
 # Adding arguments
@@ -113,6 +113,14 @@ parser.add_argument('-s', '--start', dest='s', metavar=('START'), nargs=1, type=
 parser.add_argument('-e', '--end', dest='e', metavar=('END'), nargs=1, type=int, help='A ending index')
 parser.add_argument('-out', '--output', dest='output', metavar=('OUTPUT'), nargs=1, type=str, help='output file')
 parser.add_argument('-n', '--number', dest='number', action='store_true', help='return number curve')
+
+
+
+#if sys.stdout.isatty():
+#    print("You're running in a real terminal")
+#else:
+#    print("You're being piped or redirected")
+
 
 # Parsing arguments
 args = parser.parse_args()
@@ -123,7 +131,7 @@ end=0
 if (args.number):
     print(nb)
     exit()
-    
+
 if (args.e is None):
     end = nb
 else:
@@ -131,7 +139,7 @@ else:
         end=nb
     else:
         end=args.e[0]
-        
+
 if (args.s is None):
     start=0
 else:
