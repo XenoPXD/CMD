c79bce9998f693570c57187180a9b4fc12d68455
XenoPXD <pxd2001@yahoo.fr>
2020-07-15 15:19:12 +0200

    Add files via upload

---
+++ divers/Net-Force/Escape now!!!/Escape now!!!.js
@@ -0,0 +1,12 @@
+
+
+function validate(text1,text2,text3,text4)
+{
+ if (text1==text2 && text3==text4)
+ alert("Well done use this password on the challenge page");
+ else 
+ {
+  alert("Wrong...!");
+ }
+}
+
